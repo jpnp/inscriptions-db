7a62d9f67df5f3f03c58049f4359245af474e37c
John Pybus <john.pybus@oerc.ox.ac.uk>
2023-09-21 14:58:02 +0100

    Load real data

--- load.sh
+++ load.sh
@@ -2,7 +2,7 @@
 
 CORE=foo
 curl http://localhost:8983/solr/${CORE}/update -H "Content-Type: text/xml" \
-	--data @data.xml
+	--data @ionia.xml
 
 curl http://localhost:8983/solr/${CORE}/update -H "Content-Type: text/xml" \
 	--data '<commit/>'
--- testapp/search.py
+++ testapp/search.py
@@ -7,6 +7,6 @@ def results():
     corename = current_app.config["SOLR_CORE"]
 
     solr = pysolr.Solr(corename)
-    results = solr.search('city:Sardis')
-    #results = solr.search('*')
+    #results = solr.search('city:Sardis')
+    results = solr.search('*',rows=3000)
     return render_template('results.html', res=list(results))
